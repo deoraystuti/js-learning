92ca39ee37ef73f56436b0283410e5784a483904
Stuti Deoray <116909511+deoraystuti@users.noreply.github.com>
2025-08-24 12:06:48 +0000

    dataType in javascript

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+/* 
+ In early javaScript there was NO classes, module, arrow function . but now it is so we don't want to mess the old code 
+ so, to provide the standardization we use "use strict"; // now after using this every code will be considered as newer
+ version of javascript
+*/
+
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3); we are using nodejs , not browser
+
+let name = "Stuti";  // String
+let age = 22; //  number
+let isLoggedIn = false;   // boolean
+
+//number => 2 to power 53
+// bigint                           typeof
+// string => ""                     typeof string
+// boolean => true / false          typeof boolean
+// null => standalone value         typeof object
+// undefined => not define yet.     typeof undefined
+// symbol => to use uniqueness 
+
+// object 
+
+console.log(typeof bigint);
